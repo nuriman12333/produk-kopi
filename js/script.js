236872f01d55584a214fa223323e53539ba60570
nuriman <nuriman.909090@gmail.com>
2024-01-22 13:25:40 +0700

    membuat website kopi kenangan senja

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Toggle kelas active
+const navbarNav = document.querySelector('.navbar-nav');
+// Ketika menu hamburger diklik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik diluar sidebar untuk keluar navbar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+    navbarNav.classList.remove('active');
+})
